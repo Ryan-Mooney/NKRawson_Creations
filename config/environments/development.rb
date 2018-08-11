414cfb70c2aeb198e8d4ca55fa1880aea7dbb296
EC2 Default User <ec2-user@ip-172-31-9-222.us-east-2.compute.internal>
2018-08-11 20:46:31 +0000

    removed login credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,21 +60,12 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
   #Sets up configurations to use mailgun
-  ActionMailer::Base.smtp_settings = {
-    :authentication => :plain,
-    :address => "smtp.mailgun.org",
-    :port => 587,
-    :domain => "sandbox0a8b787e4ffd4190bc8da03ef5cfce31.mailgun.org",
-    :user_name => "postmaster@sandbox0a8b787e4ffd4190bc8da03ef5cfce31.mailgun.org",
-    :password => "442bbbe79b36285edeb5eb6394771bd7-7efe8d73-1fc84ce6"
-}
-
 ActionMailer::Base.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'shrouded-fortress-66409.herokuapp.com',
+  :domain         => 'sandbox0a8b787e4ffd4190bc8da03ef5cfce31.mailgun.org',
   :authentication => :plain,
 }
 
