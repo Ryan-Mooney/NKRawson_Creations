393c6d46ac0e6fef2ca147b9338485d537eb0c50
Ryan Mooney <mooneyryanj@gmail.com>
2018-08-11 15:57:18 -0500

    updated prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Sets up configurations to use mailgun
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'sandbox0a8b787e4ffd4190bc8da03ef5cfce31.mailgun.org',
+  :authentication => :plain,
+}
+
+#config.action_mailer.default_url_options - { host: "localhost:3000" }
 end
